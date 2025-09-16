6fd3a0e03dad53c8a02bb4ef3b585c3eb3743cdc
kafues511 <48208448+kafues511@users.noreply.github.com>
2025-09-16 22:13:57 +0900

    bUsePrecompiledだけでいいかも

--- Source/ShaderHotReload/ShaderHotReload.Build.cs
+++ Source/ShaderHotReload/ShaderHotReload.Build.cs
@@ -6,31 +6,6 @@ public class ShaderHotReload : ModuleRules
 {
 	public ShaderHotReload(ReadOnlyTargetRules Target) : base(Target)
 	{
-		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-
 		bUsePrecompiled = true;
-
-		PublicDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"Core",
-			}
-			);
-
-		PrivateDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"Projects",
-				"InputCore",
-				"EditorFramework",
-				"UnrealEd",
-				"ToolMenus",
-				"CoreUObject",
-				"Engine",
-				"Slate",
-				"SlateCore",
-				"RenderCore",
-			}
-			);
 	}
 }
